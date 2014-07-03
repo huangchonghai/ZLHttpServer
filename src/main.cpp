06471f394e8ebc20d03b9e2eeafb0f8bcfbaac96
lizhenghn123 <lizhenghn@gmail.com>
2014-07-03 22:52:20 +0800

    rename
    Signed-off-by: lizhenghn123 <lizhenghn@gmail.com>

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,20 @@
 #include <iostream>
-#include "HttpLog.h"
+#include "Log.h"
+#include <stdio.h>
+#include <time.h>
+#include <string.h>
+
+void test_log()
+{
+	ZL::g_httplog.Print("Hello World [%d]\n", 1000);
+
+	INTIALISE_LOG("HttpServer2.log");
+	LOG_TRACE("Hello World [%d]", 1000);
+	LOG_TRACE("Hello World [%d][%s]", 1000, "ddddert");
+}
 
 int main()
 {
-	ZL::g_httplog.Print("Hello World [%d]\n", 1000);
 
 	return 0;
 }
